ed40539bbba407ad5aca0a9ef51deccf54f8d8de
zhangboyang <zhangby25@mails.tsinghua.edu.cn>
2026-01-06 15:44:07 +0800

    checkpoint

---
+++ run_dev_msmarco.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+export CUDA_VISIBLE_DEVICES=5
+
+TRAIN_DIR=results/arguana/queryT_linear/lr1e-5_ep5_fbFalse_hnw0.0_temp0.02/20260102-221551
+
+TS=$(date +"%Y%m%d-%H%M%S")
+OUT_DIR=test_results/arguana_cos_only/dev/${TS}
+
+mkdir -p ${OUT_DIR}
+
+python test_contradiction_faiss_final.py \
+  --dataset_name arguana \
+  --split dev \
+  --model_name_or_path ${TRAIN_DIR}/model \
+  --write_path ${OUT_DIR} \
+  --pooler_type avg \
+  --max_seq_length 512 \
+  --batch_size 64 \
+  --k_neighbors 1000 \
+  --use_query_transform True \
+  --query_transform_scale 1.0 \
+  2>&1 | tee ${OUT_DIR}/dev_eval.txt
+
+cp ${OUT_DIR}/dev_eval.txt \
+   ${TRAIN_DIR}/dev_eval.txt
